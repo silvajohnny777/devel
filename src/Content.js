e4addc8577580f78cdfdf38cb12d8c2f99945949
silvajohnny777 <jonatha_silva2012@hotmail.com>
2020-02-23 00:34:42 -0300

    sdgdfhfg

--- src/Content.js
+++ src/Content.js
@@ -11,20 +11,24 @@ import React from 'react';
                         {
                             id: 1,
                             title: 'Product',
-                            description: 'Description'
+                            description: 'Templates',
+                            longerDescription: 'Many modern and good looking templates for your VAM based website.'
                         },
                         {
                             id: 2,
                             title: 'Data',
-                            description: 'Description'
+                            description: 'Real airlines routes',
+                            longerDescription: 'Real world and updated data from airlines API. Get it on .csv file and upload to your database!'
                         },
                         {
                             id: 3,
                             title: 'Getting started',
-                            description: 'Description'
+                            description: 'Description',
+                            longerDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
                         }
                     ],
-                    boxSelecionado: '',
+                    boxSelected: '',
+                    presentation: 'Virtual LH has now many products for the virtual aviation world'
                         
                 }          
 
@@ -32,16 +36,16 @@ import React from 'react';
 
         selecionaBox = (id) => {
 
-            this.state.boxSelecionado == id ?
+            this.state.boxSelected == id ?
             
             this.setState({
-                boxSelecionado: ''
+                boxSelected: ''
             })
 
             :
             
             this.setState({
-                boxSelecionado: id
+                boxSelected: id
             })
             
         } 
@@ -50,12 +54,14 @@ import React from 'react';
 
             return (
 
+                <>
+
                 <div className="contentContainer">
 
                     {this.state.box.map((box) => {
                         return (
 
-                            <div className="actionBox" style={{marginTop: this.state.boxSelecionado === box.id ? '-2%' : '', borderTop: this.state.boxSelecionado === box.id ? '5px solid rgb(5, 22, 77)' : '', borderRadius: this.state.boxSelecionado === box.id ? '5px' : ''}} key={box.id} onClick={() => this.selecionaBox(box.id)}>
+                            <div className="actionBox" style={{marginTop: this.state.boxSelected === box.id ? '-2%' : '', borderTop: this.state.boxSelected === box.id ? '5px solid rgb(5, 22, 77)' : '', borderRadius: this.state.boxSelected === box.id ? '5px' : ''}} key={box.id} onClick={() => this.selecionaBox(box.id)}>
         
                                 <div className="actionBoxTitle">
                                     {box.title}
@@ -71,6 +77,43 @@ import React from 'react';
                     })}
 
                 </div>
+
+                {
+
+                    this.state.boxSelected ? 
+
+                        <div className="contentContainerColumn">
+        
+                            <div className="selectedTitle">
+
+                                {this.state.box[this.state.boxSelected-1].description}:
+        
+                            </div>
+        
+                            <div className="selectedSubtitle">
+
+                                {this.state.box[this.state.boxSelected-1].longerDescription}
+
+                            </div>
+        
+                        </div>                    
+                         
+                    
+                    : 
+
+                        <div className="contentContainer">
+        
+                            <div className="presentationText">
+        
+                                {this.state.presentation}
+        
+                            </div>
+        
+                        </div>
+
+                }
+
+                </>
 
             )
 
